608212dea1900ff400cd113623efda1445f19367
coutvv <coutvv@gmail.com>
2016-12-13 17:14:24 +0300

    Вывод в консоли на русском сделаль(UTF-8)

--- runnerLoop.sh
+++ runnerLoop.sh
@@ -4,5 +4,5 @@ FILE="target/vk-liker-jar-with-dependencies.jar"
 if [ ! -f $FILE]; then
     echo "Oops! Executing  $FILE not found!"
 else 
-    exec java -jar $FILE loop
+    exec java -jar -Dfile.encoding=UTF-8 $FILE loop
 fi
